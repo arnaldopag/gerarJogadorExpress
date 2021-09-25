45195e474ab4e616878c2cc4990143a4d4a116b0
arnaldopag <arnaldopaganijr@gmail.com>
2021-09-25 16:10:40 -0300

    alterado configuracao de producao

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = require("./routes");
-const dados = require("./dados.json");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 const cors = require("cors");
 
 app.use(cors());
 app.use(router);
-app.listen(port, function (){
+app.listen(port, function () {
     console.log(`Servidor Rodando na porta ${port}`);
 });
